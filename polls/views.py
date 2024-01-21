6a2cfbd9c5a861c831ba0274bcaa9d26d2cfeb1f
daigo <Nakashima6630@gmail.com>
2024-01-21 23:44:45 +0900

    '修正3'

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import SubmitAttendance
+from .forms import SubmitAttendanceForm
+from datetime import datetime
+from django.utils import timezone
 
 # Create your views here.
-def index(request):
-    return HttpResponse("出勤,退勤")
+# def index(request):
+#     return HttpResponse("出勤,退勤")
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
+        form = SubmitAttendanceForm
+        context = {
+            'form': form,
+            "user": request.user,
+        }
         return render(request, "polls/index.html")
+index = IndexView.as_view()
+
+class ResultView(View):
+    def post(self, request):
+        form = SubmitAttendanceForm(request.POST)
+        now = datetime.now()
+        month = now.month
+        day = now.day
+        hour = now.hour
+        minute = now.minute
+
+        obj = form.save(commit=False)
+        obj.place = request.POST["place"]
+        obj.in_out = request.POST["in_out"]
+        obj.staff = request.user
+        obj.date = datetime.now().date()
+        obj.time = datetime.now().time()
+        obj.save()
+        if request.POST["in_out"] == '1':
+            comment = str(month) + "月" + str(day) +"日" + str(hour) + "時" + str(minute) + "分\n" + "出勤確認しました。今日も頑張りましょう！"
+        else:
+            comment = str(month) + "月" + str(day) +"日" + str(hour) + "時" + str(minute) + "分\n" + "退勤確認しました。お疲れ様でした(^-^)！"
+        context = {
+            'place': SubmitAttendance.PLACES[int(obj.place)-1][1],
+            'comment': comment,
+        }
+        return render(request, 'polls/result.html', context)
+result = ResultView.as_view()
